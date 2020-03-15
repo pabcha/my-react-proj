3884ad7ca8134e1e54913f8e2eb29039f39b0d14
Pabcha <pablochavez15@gmail.com>
2020-03-15 15:44:16 -0300

    Starting V.2 - Removing browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,12 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
-var browserSync = require('browser-sync').create();
 
 
 gulp.task("babel", function () {
   return gulp.src("src/app.js")
     .pipe(babel())
-    .pipe(gulp.dest("scripts"))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest("scripts"));
 });
 
-gulp.task('serve', function() {
-
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("src/*.js", gulp.series('babel'));
-    gulp.watch("*.html").on('change', browserSync.reload);
-});
-
-
-gulp.task('default', gulp.series('babel', 'serve'));
+gulp.task('default', gulp.series('babel'));
 
