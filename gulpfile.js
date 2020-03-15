b14363f1d95d79404b7517e4efc300024bb24bdb
Pabcha <pablochavez15@gmail.com>
2020-03-15 15:51:52 -0300

    Replacing gulp-babel with babelify and browserify (for module creation)

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require("gulp");
-var babel = require("gulp-babel");
-
+//var babel = require("gulp-babel");
+var babelify = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task("babel", function () {
-  return gulp.src("src/app.js")
-    .pipe(babel())
-    .pipe(gulp.dest("scripts"));
+    return browserify({
+        entries: ["./src/app.js"]
+    })
+    .transform(babelify.configure({}))
+    .bundle()
+    .pipe(source("bundle.js"))
+    //.pipe(buffer())
+    //.pipe(uglify())
+    .pipe(gulp.dest("./scripts"));
 });
 
 gulp.task('default', gulp.series('babel'));
